14afdab1e5089e9fe44ea872b0504767a36ecb45
杨家馨 <578755236@qq.com>
2020-01-16 16:46:20 +0800

    react路由

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
-export default class index extends Component {
+export default class Index extends Component {
     componentDidMount() {
         
     }
     render() {
         return (
             <div>
-                
+                woshi首页
             </div>
         )
     }
--- src/router/router.js
+++ src/router/router.js
@@ -1,2 +1,16 @@
 import React, {Component } from 'react';
-import { Switch,Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
+import Index from '../pages/index/index'
+class RootLayout  extends Component {
+    render() {
+        return (
+            <div className="root-layout">
+                <Switch>
+                    <Route path='/' component={Index}></Route>
+                    <Route path='/Index' component={Index}></Route>
+                </Switch>
+            </div>
+        )
+    }
+}
+export default RootLayout;
